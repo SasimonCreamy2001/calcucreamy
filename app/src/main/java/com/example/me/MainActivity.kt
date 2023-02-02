7cce191b16314a6664c2c2541a5f9b59d42f6e90
SASIVIMON_WORK3 <646051200137@mail.rmutk.ac.th>
2023-02-02 22:52:37 +0700

    calcucreamy

--- app/src/main/java/com/example/me/MainActivity.kt
+++ app/src/main/java/com/example/me/MainActivity.kt
@@ -2,129 +2,71 @@ package com.example.me
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    var indep = true
+    var oldnum = ""
+    var opera = "+"
+    fun numberevent(view: View) {
+        if (indep)
+            text_v.setText("")
+        indep = false
+        var buclick :String = text_v.text.toString()
+        var buselect = view as Button
+        when(buselect.id){
+            num1.id -> {buclick +="1"}
+            num2.id -> {buclick +="2"}
+            num3.id -> {buclick +="3"}
+            num4.id -> {buclick +="4"}
+            num5.id -> {buclick +="5"}
+            num6.id -> {buclick +="6"}
+            num7.id -> {buclick +="7"}
+            num8.id -> {buclick +="8"}
+            num9.id -> {buclick +="9"}
+            num0.id -> {buclick +="0"}
+            point.id -> {buclick +="."}
+        }
+        text_v.setText(buclick)
+    }
 
-        var str: String
-        var temp: String
+    fun operaterevent(view: View) {
+        indep = true
+        oldnum = text_v.text.toString()
+        var  buselect : Button = view as Button
+        when(buselect.id){
+            multiplies.id -> {opera ="*"}
+            plus.id -> {opera ="+"}
+            minus.id -> {opera ="-"}
+            divided.id -> {opera ="/"}
+    }
 
-        num1.setOnClickListener {
-            temp = "1"
-            if (text_v.text.toString().toInt() == 0) {
-                text_v.setText(temp)
-            } else {
-                str = text_v.text.toString().plus(temp)
-                text_v.setText(str)
-            }
+}
+    fun equalEvent(view: View) {
+        var newnumber :String = text_v.text.toString()
+        var result = 0.00
+        when(opera){
+            "+" -> {result = oldnum.toDouble() + newnumber.toDouble()}
+            "*" -> {result = oldnum.toDouble() * newnumber.toDouble()}
+            "/" -> {result = oldnum.toDouble() / newnumber.toDouble()}
+            "-" -> {result = oldnum.toDouble() - newnumber.toDouble()}
+        }
+        text_v.setText(result.toString())
+}
 
-        }
-        clear.setOnClickListener {
-            text_v.setText("0")
-        }
-        num2.setOnClickListener {
-            temp = "2"
-            if (text_v.text.toString().toInt() == 0) {
-                text_v.setText(temp)
-            } else {
-                str = text_v.text.toString().plus(temp)
-                text_v.setText(str)
-            }
-        }
-        num3.setOnClickListener {
-            temp = "3"
-            if (text_v.text.toString().toInt() == 0) {
-                text_v.setText(temp)
-            } else {
-                str = text_v.text.toString().plus(temp)
-                text_v.setText(str)
-            }
-        }
-        num0.setOnClickListener {
-            temp = "0"
-            if (text_v.text.toString().toInt() == 0) {
-                text_v.setText(temp)
-            } else {
-                str = text_v.text.toString().plus(temp)
-                text_v.setText(str)
-            }
-        }
-        num4.setOnClickListener {
-            temp = "4"
-            if (text_v.text.toString().toInt() == 0) {
-                text_v.setText(temp)
-            } else {
-                str = text_v.text.toString().plus(temp)
-                text_v.setText(str)
-            }
-        }
-        num5.setOnClickListener {
-            temp = "5"
-            if (text_v.text.toString().toInt() == 0) {
-                text_v.setText(temp)
-            } else {
-                str = text_v.text.toString().plus(temp)
-                text_v.setText(str)
-            }
-        }
-        num6.setOnClickListener {
-            temp = "6"
-            if (text_v.text.toString().toInt() == 0) {
-                text_v.setText(temp)
-            } else {
-                str = text_v.text.toString().plus(temp)
-                text_v.setText(str)
-            }
-        }
-        num7.setOnClickListener {
-            temp = "7"
-            if (text_v.text.toString().toInt() == 0) {
-                text_v.setText(temp)
-            } else {
-                str = text_v.text.toString().plus(temp)
-                text_v.setText(str)
-            }
-        }
-        num8.setOnClickListener {
-            temp = "8"
-            if (text_v.text.toString().toInt() == 0) {
-                text_v.setText(temp)
-            } else {
-                str = text_v.text.toString().plus(temp)
-                text_v.setText(str)
-            }
-        }
-        num9.setOnClickListener {
-            temp = "9"
-            if (text_v.text.toString().toInt() == 0) {
-                text_v.setText(temp)
-            } else {
-                str = text_v.text.toString().plus(temp)
-                text_v.setText(str)
-            }
-        }
-        num1.setOnClickListener {
-            temp = "1"
-            if (text_v.text.toString().toInt() == 0) {
-                text_v.setText(temp)
-            } else {
-                str = text_v.text.toString().plus(temp)
-                text_v.setText(str)
-            }
-        }
-        delete.setOnClickListener{
-            if (text_v.text.length !=1) {
-                str = text_v.text.toString().dropLast(1)
-                text_v.setText(str)
-            }
-            else{
-                str = "0"
-                text_v.setText(str)
-            }
+    fun acEvent(view: View) {
+        text_v.setText("0")
+        indep = true
+    }
 
-        }
+    fun percentEvent(view: View) {
+        var no: Double = text_v.text.toString().toDouble()/100
+        text_v.setText(no.toString())
+        indep = true
     }
 }
+
+
